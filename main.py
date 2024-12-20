95b7d3c766d2fba7bdb909d06e6a9df1e7584a16
mehdi el kouhen <mehdi.elkouhen@externe.systeme-u.fr>
2024-12-20 11:23:10 +0100

    wip

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "bonjour {}!".format(name)
+    return "hello {}!".format(name)
 
 
 if __name__ == "__main__":
