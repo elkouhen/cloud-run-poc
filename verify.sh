a7e57aa081167bdbdb7e796a2ab8d1b8bde51360
mehdi el kouhen <mehdi.elkouhen@externe.systeme-u.fr>
2024-12-30 15:05:01 +0100

    wip

---
+++ verify.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apk update
+apk install jq curl
+
+ACCESS_TOKEN=`curl -H "Metadata-Flavor: Google"  http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app`
+
+curl -H "Authorization: Bearer $ACCESS_TOKEN" https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app/|grep -i hello
