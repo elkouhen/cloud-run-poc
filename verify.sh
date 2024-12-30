2983943061c85887aedfe2eb0c1ce15141bea307
mehdi el kouhen <mehdi.elkouhen@externe.systeme-u.fr>
2024-12-30 16:53:14 +0100

    wip

--- verify.sh
+++ verify.sh
@@ -5,4 +5,4 @@ apt install jq curl -y
 
 ACCESS_TOKEN=`curl -H "Metadata-Flavor: Google"  http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app`
 
-curl -cH "Authorization: Bearer $ACCESS_TOKEN" https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app/|grep -i hello
+curl -H "Authorization: Bearer $ACCESS_TOKEN" https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app/|grep -i hello
