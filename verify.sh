372a7b3739cc94bf7919cc52663f6dfd22ba4fc8
mehdi el kouhen <mehdi.elkouhen@externe.systeme-u.fr>
2024-12-31 13:54:52 +0100

    setup authent wif

--- verify.sh
+++ verify.sh
@@ -3,6 +3,8 @@
 apt update
 apt install jq curl -y
 
+curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/ -H "Metadata-Flavor: Google" -v
+
 ACCESS_TOKEN=`curl -v -H "Metadata-Flavor: Google"  http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app`
 
 echo "ACCESS_TOKEN $ACCESS_TOKEN"
