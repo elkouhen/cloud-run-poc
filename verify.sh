f9f769512e3dbc140364440dc0741fd9d79151a6
mehdi el kouhen <mehdi.elkouhen@externe.systeme-u.fr>
2024-12-31 11:24:05 +0100

    setup authent wif

--- verify.sh
+++ verify.sh
@@ -5,4 +5,9 @@ apt install jq curl -y
 
 ACCESS_TOKEN=`curl -H "Metadata-Flavor: Google"  http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app`
 
-curl -H "Authorization: Bearer $ACCESS_TOKEN" https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app/|grep -i hello
+echo $ACCESS_TOKEN
+RESULT=`curl -H "Authorization: Bearer $ACCESS_TOKEN" https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app/`
+
+echo $RESULT
+
+echo $RESULT|grep -i hello
