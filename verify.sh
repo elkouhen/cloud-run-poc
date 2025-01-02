03f222712b0e62dcc974a8de37a06aa773ac7ce2
mehdi el kouhen <mehdi.elkouhen@externe.systeme-u.fr>
2025-01-02 13:52:26 +0100

    setup authent wif

--- verify.sh
+++ verify.sh
@@ -8,7 +8,7 @@ curl http://metadata.google.internal/computeMetadata/v1/instance/service-account
 ACCESS_TOKEN=`curl -v -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app`
 
 echo "ACCESS_TOKEN $ACCESS_TOKEN"
-RESULT=`curl -v -H "Authorization: Bearer $ACCESS_TOKEN" http://10.0.0.4:80/`
+RESULT=`curl -v -H "Authorization: Bearer $ACCESS_TOKEN" https://cloud-run-poc-service-dev-644036867588.europe-west1.run.app`
 
 echo "RESULT $RESULT"
 
